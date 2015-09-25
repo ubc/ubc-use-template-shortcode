09d471418c53366fa8e89a04266977d28a494613
Bill Pickard <bill.r.pickard@gmail.com>
2015-09-25 10:55:20 -0700

    ETS Isotope template
    PHP template to fix custom isotope implementation on
    http://ets.educ.ubc.ca

---
+++ templates/education/ets.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * A template for Faculty of Education html within Loop or Isotope queries. Edit as needed.
+ */
+
+// Fetch the post ID for the currently set up post - we're in the loop
+$post_id = get_the_ID();
+
+// Some custom fields
+$session_type = get_post_meta( absint( $post_id ), 'session_type', true );
+
+// Replacement for [plain_cat_slug]
+$category = get_the_category();
+
+$plain_cat_slug = '';
+
+for($i=0;$i<count($category);$i++){
+	$plain_cat_slug .= $category[$i]->slug . " ";
+}
+
+?>
+
+
+	<div class="boxey <?php echo esc_attr( $plain_cat_slug ); ?> boxStyle <?php echo esc_attr( $session_type ); ?> ">
+		<p><?php echo esc_html($plain_cat_slug); ?> <span class="sessionType"><?php echo esc_html($session_type); ?></span></p>
+		<h3 class="boxTitle"><a title="<?php echo esc_attr( the_title() ); ?>" href="<?php echo esc_attr( the_permalink()); ?>" target="_blank"><?php esc_html( the_title() ); ?></h3>
+		<p class="boxContent"><?php echo esc_html( the_excerpt() ); ?></p>
+	</div>
