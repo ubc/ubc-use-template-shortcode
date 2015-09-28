b82e4a9f33ecff74966baa8060f4a6bfd89faeb1
Bill Pickard <bill.r.pickard@gmail.com>
2015-09-28 08:43:09 -0700

    Minor updates to clean up for merging
    * Added URL where template will be deployed
    * Formatting changes to meet Wordpress Coding Standards
    * Changed one instance of esc_attr() to esc_url()

--- templates/education/ets.php
+++ templates/education/ets.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * A template for Faculty of Education html within Loop or Isotope queries. Edit as needed.
+ * A template for Faculty of Education html within Loop or Isotope queries.  
+ * This template is specifically for http://ets.educ.ubc.ca/learning-technology-events
  */
 
 // Fetch the post ID for the currently set up post - we're in the loop
@@ -15,15 +16,15 @@ $category = get_the_category();
 
 $plain_cat_slug = '';
 
-for($i=0;$i<count($category);$i++){
-	$plain_cat_slug .= $category[$i]->slug . " ";
+for ( $i=0; $i<count( $category ); $i++) {
+	$plain_cat_slug .= $category[ $i ]->slug . " ";
 }
 
 ?>
 
 
 	<div class="boxey <?php echo esc_attr( $plain_cat_slug ); ?> boxStyle <?php echo esc_attr( $session_type ); ?> ">
-		<p><?php echo esc_html($plain_cat_slug); ?> <span class="sessionType"><?php echo esc_html($session_type); ?></span></p>
-		<h3 class="boxTitle"><a title="<?php echo esc_attr( the_title() ); ?>" href="<?php echo esc_attr( the_permalink()); ?>" target="_blank"><?php esc_html( the_title() ); ?></h3>
+		<p><?php echo esc_html( $plain_cat_slug ); ?> <span class="sessionType"><?php echo esc_html( $session_type ); ?></span></p>
+		<h3 class="boxTitle"><a title="<?php echo esc_attr( the_title_attribute() ); ?>" href="<?php echo esc_url( the_permalink()); ?>" target="_blank"><?php esc_html( the_title() ); ?></h3>
 		<p class="boxContent"><?php echo esc_html( the_excerpt() ); ?></p>
 	</div>
