f60e251f89ca58d19b5bba43c0f6e6edbb62fad9
gdouglas <grm.dgs@gmail.com>
2015-09-30 12:54:19 -0700

    Using helper instead of getting categories

--- templates/medicine/div-with-cat-slugs.php
+++ templates/medicine/div-with-cat-slugs.php
@@ -6,8 +6,13 @@
  * This is a simple template to allow for larger reusability. Outputs a div with an id of the post. 
  *
  */
+// Fetch the post ID for the currently set up post - we're in the loop
+$post_id 		= get_the_ID();
 
+// Replacement for [plain_term_slug]
+$plain_term_slug 	= \UBC\Helpers::get_plain_terms( absint( $post_id ), 'slug', ' ' );
 ?>
-<div class=" boxey <?php foreach(get_the_category() as $category) {
-echo esc_attr( $category->slug . ' ' );} ?>">
+
+
+<div class="boxey <?php echo esc_attr( $plain_term_slug ); ?>">
 
