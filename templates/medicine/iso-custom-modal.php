2b4e9690516e7da8ba6732601898190a6cfc8d75
gdouglas <grm.dgs@gmail.com>
2015-09-30 15:07:49 -0700

    escaped variables
    Escaped variables when used

--- templates/medicine/iso-custom-modal.php
+++ templates/medicine/iso-custom-modal.php
@@ -1,58 +1,56 @@
 <?php 
 /**
  * URL: http://arts.med.ubc.ca/
- * Custom modal launch button for ISO shortcode usage. 
+ * Custom loop and modal for ISO shortcode usage. 
  * 
- * This is a simple template to allow for larger reusability. Outputs a div with an id of the post. 
  *
  */
 
-$post_id 			= absint( get_the_ID() );
-$post_thumbnail 	= wp_get_attachment_url( get_post_thumbnail_id( $post_id ) );
-$the_permalink		= esc_url( get_permalink() );
-$the_title          = esc_html( get_the_title() );
+$post_id 			= get_the_ID();
+$post_thumbnail 	= wp_get_attachment_url( get_post_thumbnail_id( absint( $post_id ) ) );
+$the_permalink		= get_permalink();
+$the_title          = get_the_title();
 $the_date 			= get_the_date();
-$the_excerpt 		= wp_kses_post( get_the_excerpt() );
-$the_content        = wp_kses_post( get_the_content() );
-$category_list		= wp_kses_post( get_the_category_list() );
-$the_author			= esc_html( get_the_author() );
-$plain_tags_slug 	= \UBC\Helpers::get_plain_tags( absint( $post_id ), 'slug', ' ' );
+$the_excerpt 		= get_the_excerpt();
+$the_content        = get_the_content();
+$category_list		= get_the_category_list();
+$the_author			= get_the_author();
 
 ?>
 
 
 
 	<div class="boxey <?php echo esc_attr( $plain_term_slug ); ?>">
-		<small class="header-tags"><?php echo $category_list; ?></small>
+		<small class="header-tags"><?php echo wp_kses_post( $category_list ); ?></small>
 
-		<img src="<?php echo $post_thumbnail; ?>" class="img-rounded">
-		<h2><a href="<?php echo $the_permalink; ?>" title="<?php echo $the_title; ?>"> <?php echo $the_title; ?></a></h2>
+		<img src="<?php echo esc_url( $post_thumbnail ); ?>" class="img-rounded">
+		<h2><a href="<?php echo esc_url( $the_permalink ); ?>" title="<?php echo esc_html( $the_title ); ?>"> <?php echo esc_html( $the_title ); ?></a></h2>
 		<p class="the_author">
-			<?php echo $the_author; ?><br />
-			<small><?php echo $the_date; ?></small>
+			<?php echo esc_html( $the_author ); ?><br />
+			<small><?php echo esc_html( $the_date );?></small>
 		</p>
 		
-		<a class="btn btn-small launch-btn" href="#<?php echo absint( $post_id ); ?>" data-toggle="modal">Launch</a>
-		<p class="the_excerpt"><?php echo $the_excerpt ?></p>
+		<a class="btn btn-small launch-btn" title="launch post" href="#<?php echo absint( $post_id ); ?>" data-toggle="modal">Launch</a>
+		<p class="the_excerpt"><?php echo wp_kses_post( $the_excerpt ); ?></p>
 
 
-		<div id="<?php echo $post_id; ?>" class="modal fade hide container" tabindex="-1">
+		<div id="<?php echo absint( $post_id ); ?>" class="modal fade hide container" tabindex="-1">
 			<div>
 				<div class="modal-header">
 					<button class="close" type="button" data-dismiss="modal">×</button>
-					<h3><a href="<?php echo $the_permalink; ?>#myModalLablel_" class="modal-label header-tags"><?php echo $the_title; ?></a></h3>
+					<h3><a href="<?php echo esc_url( $the_permalink ); ?>#myModalLablel_" title="<?php echo esc_html( $the_title ); ?>" class="modal-label header-tags"><?php echo esc_html( $the_title ); ?></a></h3>
 				</div>
 				<!-- end #modal-header -->
 				<div class="modal-body">
 					<div class="row-fluid">
-						<div class="modal-body-content"><?php echo $the_content;  ?></div>
+						<div class="modal-body-content"><?php echo wp_kses_post( $the_content );  ?></div>
 					</div>
 				<!-- end .row-fluid -->
 
 				</div>
 				<!-- end .modal-body -->
 				<div class="modal-footer">
-					<a href="<?php echo $the_permalink; ?>">open full page <i class="icon-share-alt belize-hole"></i></a>
+					<a href="<?php echo esc_url( $the_permalink ); ?>" title="Open full page">open full page <i class="icon-share-alt belize-hole"></i></a>
 					<button class="close" type="button" data-dismiss="modal">×</button>
 				</div>
 				<!-- End modal-footer -->
