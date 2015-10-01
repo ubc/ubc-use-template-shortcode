a1caabed0b01665d468dc48657db1c9ae34638c9
Jamie Edwards <jamie.edwards@ubc.ca>
2015-10-01 12:34:36 -0700

    Update and cleanup of initial commit

--- templates/oksummer/iso-thumb-date-excerpt.php
+++ templates/oksummer/iso-thumb-date-excerpt.php
@@ -8,17 +8,8 @@
 // Fetch the post ID for the currently set up post - we're in the loop
 $post_id 			= get_the_ID();
 
-// Some custom fields
-// $location			= get_post_meta( absint( $post_id ), 'location', true );
-
-// How to get the post thumbnail
-$post_thumbnail 	= wp_get_attachment_url( get_post_thumbnail_id( absint( $post_id ) ) );
-
 // Replacement for [plain_tags_slug]
 $plain_tags_slug 	= \UBC\Helpers::get_plain_tags( absint( $post_id ), 'slug', ' ' );
-
-// Replacement for [plain_cats_slug]
-$plain_cats_slug 	= \UBC\Helpers::get_plain_cats( absint( $post_id ), 'slug', ' ' );
 
 // Replacement for [odd-even] but *really* this should be done in CSS.
 global $usage_id;
@@ -27,14 +18,10 @@ $odd_even 			= ( 0 === $usage_id % 2 ) ? 'even' : 'odd';
 
 ?>
 
-
-
-
-<div id="post-<?php echo absint( $post_id ); ?>" class="<?php echo esc_attr( $plain_cats_slug ); ?> outerfeature boxey">
-    <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php echo esc_html( $post_thumbnail ); ?></a>
+<div id="post-<?php echo absint( $post_id ); ?>" class="<?php echo esc_attr( $plain_tags_slug ); ?> outerfeature boxey">
+    <a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium'); ?></a>
     <div>
         <div>
-
             <div class="innerfeature">
                 <div>
                     <div>
@@ -42,14 +29,11 @@ $odd_even 			= ( 0 === $usage_id % 2 ) ? 'even' : 'odd';
                             <h3 class="title"><?php the_title(); ?></h3>
                         </div>
                         <strong class="target"><?php the_excerpt(); ?></strong>
-
-                        <a class="btn btn-info btn-small" title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">More Info</a>
+                        <a class="btn btn-info btn-small" title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">More Info</a>
                     </div>
                 </div>
                        <div class="iso-description"><?php the_content(); ?></div>
-                        &nbsp;
             </div>
-
         </div>
     </div>
 </div>
