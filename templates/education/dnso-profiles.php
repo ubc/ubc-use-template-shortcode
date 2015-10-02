f7c0b8952a86ff08e8503ef28dcfcea1b26e075c
cdavis-ubc <cdavis-ubc@users.noreply.github.com>
2015-10-01 20:14:16 -0700

    Update dnso-profiles.php
    added wp_kses_post to replace esc_html line 123
    added esc_html to all 'profilefield' echos

--- templates/education/dnso-profiles.php
+++ templates/education/dnso-profiles.php
@@ -1,17 +1,24 @@
 <?php
 
 //**
-//* A template for DNSO Profiles.  http://dnso.educ.ubc.ca/administrative-directory/
+ //* A template for DNSO Profiles.  http://dnso.educ.ubc.ca/administrative-directory/
+
 // Fetch the post ID for the currently set up post - we're in the loop
+
 $post_id      = get_the_ID();
+
 // How to get the post thumbnail
 $post_thumbnail   = wp_get_attachment_url( get_post_thumbnail_id( absint( $post_id ) ) );
+
 // Replacement for [plain_tags_slug]
 $plain_tags_slug  = \UBC\Helpers::get_plain_tags( absint( $post_id ), 'slug', ' ' );
+
 // Replacement for [plain_term_slug]
 $plain_term_slug  = \UBC\Helpers::get_plain_terms( absint( $post_id ), 'slug', ' ' );
+
 // To get Taxonomies as names
 $plain_term_name  = \UBC\Helpers::get_plain_terms( absint( $post_id ), 'name', ' ' );
+
 
 // Replacement for [odd-even] but *really* this should be done in CSS.
 global $usage_id;
@@ -35,6 +42,7 @@ $email               = ( isset( $profile_cct['email']['0']['email'] ) ) ? $profi
 $clone_bio           = ( isset( $profile_cct['clone_bio']['textarea'] ) ) ? $profile_cct['clone_bio']['textarea'] : false;
 $clone_office        = ( isset( $profile_cct['clone_office']['0']['text'] ) ) ? $profile_cct['clone_office']['0']['text'] : false;
 
+
 ?>
 
 <div id="<?php echo absint( $post_id ); ?>" class="<?php echo esc_attr( $plain_term_slug ); ?> iso-profile">
@@ -44,32 +52,32 @@ $clone_office        = ( isset( $profile_cct['clone_office']['0']['text'] ) ) ? 
     <div class="boxey-inside">
 
         <div class="boxey-inner">
-
+   
           <div class="iso-featured-image"><a href="#modal_<?php echo absint( $post_id ); ?>" class="fancybox-inline"><img src="<?php echo esc_url( $post_thumbnail ); ?>" alt="" /></a></div>
 
           <h3><div class="iso-title">
 
             <a href="#modal_<?php echo absint( $post_id ); ?>" class="fancybox-inline">
-            <?php if ( false !== $salutations ) : echo $salutations; endif; ?> <?php if ( false !== $first_name ) : echo $first_name; endif; ?> <?php if ( false !== $middle ) : echo $middle; endif; ?> <?php if ( false !== $last_name ) : echo $last_name; endif; ?>          
-            <small><?php if ( false !== $position ) : echo $position; endif; ?></small></a>
+            <?php if ( false !== $salutations ) : echo esc_html( $salutations ); endif; ?> <?php if ( false !== $first_name ) : echo esc_html( $first_name ); endif; ?> <?php if ( false !== $middle ) : echo esc_html( $middle ); endif; ?> <?php if ( false !== $last_name ) : echo esc_html( $last_name ); endif; ?>          
+            <small><?php if ( false !== $position ) : echo esc_html( $position ); endif; ?></small></a>
 
           </div></h3>
 
           <div class="iso-description hidden">
-            <?php if ( false !== $salutations ) : echo $salutations; endif; ?> 
-            <?php if ( false !== $first_name ) : echo $first_name; endif; ?> 
-            <?php if ( false !== $middle ) : echo $middle; endif; ?> 
-            <?php if ( false !== $last_name ) : echo $last_name; endif; ?>
-            <?php if ( false !== $position ) : echo $position; endif; ?> 
-            <?php if ( false !== $clone_name_title_room_phone_email ) : echo $clone_name_title_room_phone_email; endif; ?> 
+            <?php if ( false !== $salutations ) : echo esc_html( $salutations ); endif; ?> 
+            <?php if ( false !== $first_name ) : echo esc_html( $first_name ); endif; ?> 
+            <?php if ( false !== $middle ) : echo esc_html( $middle ); endif; ?> 
+            <?php if ( false !== $last_name ) : echo esc_html( $last_name ); endif; ?>
+            <?php if ( false !== $position ) : echo esc_html( $position ); endif; ?> 
+            <?php if ( false !== $clone_name_title_room_phone_email ) : echo esc_html( $clone_name_title_room_phone_email ); endif; ?> 
             <?php echo esc_html( $$plain_term_name ); ?>
           </div>
 
-          <?php if ( false !== $clone_room_number ) : echo $clone_room_number; endif; ?></br>
+          <?php if ( false !== $clone_room_number ) : echo esc_html( $clone_room_number ); endif; ?></br>
 
           <i class="icon-envelope"></i> <a href="mailto:<?php echo esc_attr( $email );?>"><?php if ( false !== $email ) : echo $email; endif; ?></a></br>
 
-          <i class="icon-phone-sign"></i> <?php if ( false !== $phone1 ) : echo $phone1; endif; ?> <?php if ( false !== $phone2 ) : echo $phone2; endif; ?> <?php if ( false !== $phone3 ) : echo $phone3; endif; ?></br>
+          <i class="icon-phone-sign"></i> <?php if ( false !== $phone1 ) : echo esc_html( $phone1 ); endif; ?> <?php if ( false !== $phone2 ) : echo esc_html( $phone2 ); endif; ?> <?php if ( false !== $phone3 ) : echo esc_html( $phone3 ); endif; ?></br>
 
           <a href="#modal_<?php echo absint( $post_id ); ?>" role="button" class="btn btn-small launch-btn fancybox-inline">See More</a>
 
@@ -79,7 +87,7 @@ $clone_office        = ( isset( $profile_cct['clone_office']['0']['text'] ) ) ? 
 
     </div>
 
-  <div class="fancy-hide">
+  <div style="display:none">
 
       <div id="modal_<?php echo absint( $post_id ); ?>" class="span9">
 
@@ -100,30 +108,30 @@ $clone_office        = ( isset( $profile_cct['clone_office']['0']['text'] ) ) ? 
                     <div class="modal-iso-title">
 
                        <h3>
-                       <?php if ( false !== $salutations ) : echo $salutations; endif; ?> <?php if ( false !== $first_name ) : echo $first_name; endif; ?> <?php if ( false !== $middle ) : echo $middle; endif; ?> <?php if ( false !== $last_name ) : echo $last_name; endif; ?>          
+                       <?php if ( false !== $salutations ) : echo esc_html( $salutations ); endif; ?> <?php if ( false !== $first_name ) : echo esc_html( $first_name ); endif; ?> <?php if ( false !== $middle ) : echo esc_html( $middle ); endif; ?> <?php if ( false !== $last_name ) : echo esc_html( $last_name ); endif; ?>          
                        </h3>
 
                     </div>
  
                     <div class="modal-iso-description">
-                       <?php if ( false !== $clone_room_number ) : echo $clone_room_number; endif; ?></br>
+                       <?php if ( false !== $clone_room_number ) : echo esc_html( $clone_room_number ); endif; ?></br>
 
-                       <i class="icon-envelope"></i> <a href="mailto:<?php echo esc_attr( $email );?>"><?php if ( false !== $email ) : echo $email; endif; ?></a>
-                       <i class="icon-phone-sign"></i> <?php if ( false !== $phone1 ) : echo $phone1; endif; ?> <?php if ( false !== $phone2 ) : echo $phone2; endif; ?> <?php if ( false !== $phone3 ) : echo $phone3; endif; ?>
+                       <i class="icon-envelope"></i> <a href="mailto:<?php if ( false !== $email ) : echo esc_attr( $email ); endif; ?>"><?php if ( false !== $email ) : echo esc_html( $email ); endif; ?></a>
+                       <i class="icon-phone-sign"></i> <?php if ( false !== $phone1 ) : echo esc_html( $phone1 ); endif; ?> <?php if ( false !== $phone2 ) : echo esc_html( $phone2 ); endif; ?> <?php if ( false !== $phone3 ) : echo esc_html( $phone3 ); endif; ?>
                     </div>
   
-                    <div class="modal-postion-description"><?php if ( false !== $clone_bio ) : echo $clone_bio; endif; ?></div>
+                    <div class="modal-postion-description"><?php if ( false !== $clone_bio ) : echo wp_kses_post( $clone_bio ); endif; ?></div>
 
                 </div><!-- end .span9--> 
-
+                
               </div><!-- end .modal-body-content--> 
-
+           
             </div><!-- end .span12 --> 
-
+          
           </div><!-- end .row-fluid --> 
-
+        
         </div><!-- end .modal-body -->
-
+     
       </div><!-- end no class -->
 
     </div><!-- end #modal_theID-->
