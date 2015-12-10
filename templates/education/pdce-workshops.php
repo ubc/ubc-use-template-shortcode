3e63981b6589f72ca5a2a7deda168c87febaddfc
Bill Pickard <bill.r.pickard@gmail.com>
2015-12-10 09:40:05 -0800

    Optimizations

--- templates/education/pdce-workshops.php
+++ templates/education/pdce-workshops.php
@@ -6,6 +6,7 @@
 
 // Fetch the post ID for the currently set up post - we're in the loop
 $post_id 			= get_the_ID();
+$post_id            = $post_id;
 
 //Fetch post title
 $the_title          = get_the_title();
@@ -14,25 +15,25 @@ $the_title          = get_the_title();
 $permalink          = get_permalink( absint( $post_id) );
 
 // Some custom fields
-$coursecodeundergrad			= get_post_meta( absint( $post_id ), 'CourseCodeUndergrad', true );
-$coursecodegrad					= get_post_meta( absint( $post_id ), 'CourseCodeGrad', true );
-$vanityurl 						= get_post_meta( absint( $post_id ), 'VanityUrl', true );
-$credit 						= get_post_meta( absint( $post_id ), 'Credit', true );
-$promophoto 					= get_post_meta( absint( $post_id ), 'PromoPhoto', true );
-$subtitle						= get_post_meta( absint( $post_id ), 'Subtitle', true );
-$dates							= get_post_meta( absint( $post_id ), 'Dates', true );
-$location						= get_post_meta( absint( $post_id ), 'Location', true );
-$deliverymode                   = get_post_meta( absint( $post_id ), 'DeliveryMode', true );
-$programtype                    = get_post_meta( absint( $post_id ), 'ProgramType', true );
+$coursecodeundergrad			= get_post_meta( $post_id, 'CourseCodeUndergrad', true );
+$coursecodegrad					= get_post_meta( $post_id, 'CourseCodeGrad', true );
+$vanityurl 						= get_post_meta( $post_id, 'VanityUrl', true );
+$credit 						= get_post_meta( $post_id, 'Credit', true );
+$promophoto 					= get_post_meta( $post_id, 'PromoPhoto', true );
+$subtitle						= get_post_meta( $post_id, 'Subtitle', true );
+$dates							= get_post_meta( $post_id, 'Dates', true );
+$location						= get_post_meta( $post_id, 'Location', true );
+$deliverymode                   = get_post_meta( $post_id, 'DeliveryMode', true );
+$programtype                    = get_post_meta( $post_id, 'ProgramType', true );
 
 // How to get the post thumbnail
-$post_thumbnail 	= wp_get_attachment_url( get_post_thumbnail_id( absint( $post_id ) ) );
+$post_thumbnail 	= wp_get_attachment_url( get_post_thumbnail_id( $post_id ) );
 
 // Replacement for [plain_tags_slug]
-$plain_tags_slug 	= \UBC\Helpers::get_plain_tags( absint( $post_id ), 'slug', ' ' );
+$plain_tags_slug 	= \UBC\Helpers::get_plain_tags( $post_id, 'slug', ' ' );
 
 // Replacement for [plain_term_slug]
-$plain_term_slug 	= \UBC\Helpers::get_plain_terms( absint( $post_id ), 'slug', ' ' );
+$plain_term_slug 	= \UBC\Helpers::get_plain_terms( $post_id, 'slug', ' ' );
 
 // Replacement for [odd-even] but *really* this should be done in CSS.
 global $usage_id;
@@ -55,7 +56,7 @@ switch ( $deliverymode ){
 
 ?>
 
-<div id="<?php echo absint( $post_id ); ?>" class="<?php echo esc_attr( $plain_term_slug ); ?> iso-box">
+<div id="<?php echo $post_id; ?>" class="<?php echo esc_attr( $plain_term_slug ); ?> iso-box">
 
 	<div class="boxey entry-content  <?php echo esc_attr( $odd_even ); ?> <?php echo esc_attr( $plain_term_slug ); ?> iso-style">
 
@@ -63,7 +64,7 @@ switch ( $deliverymode ){
 
 			<div class="boxey-inner">
 
-				<a href="<?php echo esc_html( $permalink ); ?>" target="_blank"><span class="iso-title"><?php echo esc_html( $the_title ); ?></span></a>
+				<a href="<?php echo esc_url( $permalink ); ?>" target="_blank"><span class="iso-title"><?php echo esc_html( $the_title ); ?></span></a>
 
 				<div class="iso-description hidden"><?php echo wp_kses_post( get_the_excerpt() ); ?> <?php echo esc_html( $the_title ); ?> <?php echo esc_html( $coursecodeundergrad ); ?> <?php echo esc_html( $coursecodegrad ); ?> <?php echo esc_attr( $plain_term_slug ); ?> <?php echo esc_html ( $deliverymode ); ?> <?php echo esc_html ( $location ); ?> <?php echo esc_html ( $programtype ); ?> <?php echo esc_html ( $credit ); ?></div>
 
